48514b102f1c3f3afe16718ad3c6b3cb418938b9
Hilmi Azizi <root@hilmiazizi.com>
2025-07-13 09:57:08 +0700

    Add .gitignore

---
+++ example.py
@@ -0,0 +1,21 @@
+from tokopaedi import search, SearchFilters, get_product, get_reviews, combine_data
+from dataclasses import dataclass, asdict
+import json
+
+filters = SearchFilters(
+            bebas_ongkir_extra = True,
+            pmin = 15000000,
+            pmax = 30000000,
+            rt = 4.5
+        )
+
+results = search("Zenbook 14 32GB", max_result=100, debug=False)
+for result in results:
+    combine_data(
+        result,
+        get_product(product_id=result.product_id, debug=True),
+        get_reviews(product_id=result.product_id, max_result=20, debug=True)
+    )
+
+with open('log.json','w') as f:
+    f.write(json.dumps(results.json(), indent=4))
